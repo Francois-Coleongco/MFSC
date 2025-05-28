bca6976356c161cacd8481bf6d8b86c4d5ab3121
fransys <94207998+Francois-Coleongco@users.noreply.github.com>
2025-05-28 17:50:00 +0800

    Update client_default.sh

--- tests/client_default.sh
+++ tests/client_default.sh
@@ -1,9 +1,3 @@
 #!/bin/env bash
 
-../test_build/client > client_out
-
-echo henry # username
-echo camel # password
-
-echo 2 # write intention
-echo test_file # file name to write
+../test_build/client < authed_client.in 2> client.err
